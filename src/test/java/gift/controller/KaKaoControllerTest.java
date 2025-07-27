d6caa39462e7cb8d1030c38734abcde78508bb47
Dohyun <sks020k@naver.com>
2025-07-28 02:57:21 +0900

    test : API통합으로 인한 테스트코드 수정

--- src/test/java/gift/controller/KaKaoControllerTest.java
+++ src/test/java/gift/controller/KaKaoControllerTest.java
@@ -1,5 +1,6 @@
-package gift.auth.controller;
+package gift.controller;
 
+import gift.auth.controller.KakaoController;
 import gift.auth.dto.KakaoTokenResponseDto;
 import gift.auth.dto.KakaoUserInfoDto;
 import gift.auth.service.KakaoService;
@@ -52,73 +53,99 @@ class KakaoControllerTest {
   }
 
   @Test
-  void 인가_코드로_토큰_발급_성공() throws Exception {
+  void 카카오_콜백_처리_성공() throws Exception {
     // Given
-    String authCode = "valid-auth-code";
+    String code = "validCode";
+
     KakaoTokenResponseDto tokenResponse = new KakaoTokenResponseDto(
         "bearer",
         "KakaoAK.test-access-token",
         21599
     );
 
-    given(kakaoService.getAccessToken(authCode)).willReturn(tokenResponse);
-
-    // When & Then
-    mockMvc.perform(post("/kakao/token")
-            .param("code", authCode))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.message").value("토큰 발급 성공"))
-        .andExpect(jsonPath("$.tokenType").value("bearer"))
-        .andExpect(jsonPath("$.accessToken").value("KakaoAK.test-access-token"))
-        .andExpect(jsonPath("$.expiresIn").value(21599));
-  }
-
-  @Test
-  void 잘못된_인가_코드로_토큰_요청시_에러_응답() throws Exception {
-    // Given
-    String invalidCode = "strangeCode";
-    willThrow(new RuntimeException("토큰 요청 실패"))
-        .given(kakaoService).getAccessToken(invalidCode);
-
-    // When & Then
-    mockMvc.perform(post("/kakao/token")
-            .param("code", invalidCode))
-        .andExpect(status().isBadRequest())
-        .andExpect(jsonPath("$.error").value("토큰 요청 실패"))
-        .andExpect(jsonPath("$.message").value("토큰 요청 실패"));
-  }
-
-  @Test
-  void 액세스_토큰으로_사용자_정보_조회_성공() throws Exception {
-    // Given
-    String accessToken = "valid-access-token";
     KakaoUserInfoDto userInfo = new KakaoUserInfoDto(
         1L,
         new KakaoUserInfoDto.KakaoAccount("test@dohyun.com")
     );
-    given(kakaoService.getUserInfo(accessToken)).willReturn(userInfo);
+
+    String jwtToken = "jwt.test.token";
+
+    given(kakaoService.getAccessToken(code)).willReturn(tokenResponse);
+    given(kakaoService.getUserInfo(tokenResponse.accessToken())).willReturn(userInfo);
+    given(userService.handleKaKaoLogin(userInfo.getEmailSafely(), userInfo.id().toString()))
+        .willReturn(jwtToken);
 
     // When & Then
-    mockMvc.perform(post("/kakao/userinfo")
-            .param("accessToken", accessToken))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.message").value("사용자 정보 조회 성공"))
-        .andExpect(jsonPath("$.kakaoId").value(1))
-        .andExpect(jsonPath("$.email").value("test@dohyun.com"));
+    mockMvc.perform(get("/")
+            .param("code", code))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/"))
+        .andExpect(cookie().value("accessToken", jwtToken))
+        .andExpect(cookie().path("accessToken", "/"));
   }
 
   @Test
-  void 만료된_액세스_토큰으로_사용자_정보_조회시_에러_응답() throws Exception {
+  void 카카오_콜백_처리_실패시_로그인_페이지로_리다이렉트() throws Exception {
     // Given
-    String token = "expiredAccessToken";
-    willThrow(new RuntimeException("사용자 정보 요청 실패"))
-        .given(kakaoService).getUserInfo(token);
+    String invalidCode = "invalidCode";
+    willThrow(new RuntimeException("토큰 요청 실패"))
+        .given(kakaoService).getAccessToken(invalidCode);
 
     // When & Then
-    mockMvc.perform(post("/kakao/userinfo")
-            .param("accessToken", token))
-        .andExpect(status().isBadRequest())
-        .andExpect(jsonPath("$.error").value("사용자 정보 요청 실패"))
-        .andExpect(jsonPath("$.message").value("사용자 정보 요청 실패"));
+    mockMvc.perform(get("/")
+            .param("code", invalidCode))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/users/login"));
+  }
+
+  @Test
+  void 카카오_사용자_정보_조회_실패시_로그인_페이지로_리다이렉트() throws Exception {
+    // Given
+    String code = "validCode";
+    String accessToken = "valid-token";
+
+    KakaoTokenResponseDto tokenResponse = new KakaoTokenResponseDto(
+        "bearer",
+        accessToken,
+        21599
+    );
+
+    given(kakaoService.getAccessToken(code)).willReturn(tokenResponse);
+    willThrow(new RuntimeException("사용자 정보 조회 실패"))
+        .given(kakaoService).getUserInfo(accessToken);
+
+    // When & Then
+    mockMvc.perform(get("/")
+            .param("code", code))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/users/login"));
+  }
+
+  @Test
+  void JWT_토큰_생성_실패시_로그인_페이지로_리다이렉트() throws Exception {
+    // Given
+    String code = "validCode";
+
+    KakaoTokenResponseDto tokenResponse = new KakaoTokenResponseDto(
+        "bearer",
+        "valid-token",
+        21599
+    );
+
+    KakaoUserInfoDto userInfo = new KakaoUserInfoDto(
+        1L,
+        new KakaoUserInfoDto.KakaoAccount("test@dohyun.com")
+    );
+
+    given(kakaoService.getAccessToken(code)).willReturn(tokenResponse);
+    given(kakaoService.getUserInfo(tokenResponse.accessToken())).willReturn(userInfo);
+    willThrow(new RuntimeException("JWT 생성 실패"))
+        .given(userService).handleKaKaoLogin(userInfo.getEmailSafely(), userInfo.id().toString());
+
+    // When & Then
+    mockMvc.perform(get("/")
+            .param("code", code))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/users/login"));
   }
 }
